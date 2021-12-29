dfe05b83743a475950764b93a996e275febb6889
Darwin Flores <96642452+codedarwinflores@users.noreply.github.com>
2021-12-29 00:04:31 -0600

    Actualizando CDN

--- menu/header.php
+++ menu/header.php
@@ -222,7 +222,7 @@ $radios = $datos['color'];
   <!-- Inconos Font awesome -->
   <link rel="stylesheet" href="../iconos_fa/css/font-awesome.min.css">
 
-
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.0/css/font-awesome.min.css">
 
   <script src="../lib/bootstrap.min.js"></script>
 
