bf3f299053620d9b86f6d1d8cfa4e976e859663e
codedarwinflores <daflaco@hotmail.com>
2021-12-29 19:33:58 -0600

    Mi nuevo

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit0639a8dbd7f07f3f74af64b099249d05
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit0639a8dbd7f07f3f74af64b099249d05', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit0639a8dbd7f07f3f74af64b099249d05', 'loadClassLoader'));
